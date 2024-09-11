6f4da84a32def9ed2fcf172aab62806865d20e19
tonyrossett <tonyrossett@hotmail.com>
2024-09-10 23:50:07 -0300

    Created About and about-styles

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,7 +1,17 @@
+import styles from '../styles/AboutPage.module.css';
+
 const AboutPage = () => {
   return (
     <>
-      <h3>A Little Bit About Me</h3>
+      <h3 className={styles.title}>Um pouco mais de mim</h3>
+      <p className={styles.textLine1}>
+        Trabalho com tecnologia tem muito tempo e sempre gostei de fazer as coisas funcionarem, me traz uma sensação de "Missão cumprida". Desenvolver páginas e sistemas funcionais traz muito mais satisfação, motivando a buscar e fazer sempre o melhor possível.
+      </p>
+
+      <p className={styles.textLine2}>
+        Trabalhar com desenvolvimento significa atualização constante. Cuidado com os detalhes, sempre atento com a qualidade, entendendo as necessidades do cliente e suprindo cada uma delas. Isso faz parte da arte do desenvolvimento.
+      </p>
+
     </>
   );
 };
