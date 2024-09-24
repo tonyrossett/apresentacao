0c1c20695d313c804cafc31cc2f6aafa22d731b3
tonyrossett <tonyrossett@hotmail.com>
2024-09-24 19:21:17 -0300

    In About Section, text is made look like code

--- pages/about.jsx
+++ pages/about.jsx
@@ -3,13 +3,13 @@ import styles from '../styles/AboutPage.module.css';
 const AboutPage = () => {
   return (
     <>
-      <h3 className={styles.title}>Um pouco mais de mim</h3>
+      <h3 className={styles.title}>{"<h3>Um pouco mais de mim</h3>"}</h3>
       <p className={styles.textLine1}>
-        Trabalho com tecnologia tem muito tempo e sempre gostei de fazer as coisas funcionarem, me traz uma sensação de "Missão cumprida". Desenvolver páginas e sistemas funcionais traz muito mais satisfação, motivando a buscar e fazer sempre o melhor possível.
+        {"<p>Trabalho com tecnologia tem muito tempo e sempre gostei de fazer as coisas funcionarem, me traz uma sensação de Missão cumprida. Desenvolver páginas e sistemas funcionais traz muito mais satisfação, motivando a buscar e fazer sempre o melhor possível.</p>"}
       </p>
 
       <p className={styles.textLine2}>
-        Trabalhar com desenvolvimento significa atualização constante. Cuidado com os detalhes, sempre atento com a qualidade, entendendo as necessidades do cliente e suprindo cada uma delas. Isso faz parte da arte do desenvolvimento.
+        {"<p>Trabalhar com desenvolvimento significa atualização constante. Cuidado com os detalhes, sempre atento com a qualidade, entendendo as necessidades do cliente e suprindo cada uma delas. Isso faz parte da arte do desenvolvimento.</p>"}
       </p>
 
     </>
