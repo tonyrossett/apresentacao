de7401399905879debe464ba13c1485db75f265f
tonyrossett <tonyrossett@hotmail.com>
2024-10-11 00:44:09 -0300

    In About Section, formatted texts

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,16 +1,20 @@
 import styles from '../styles/AboutPage.module.css';
 
+
 const AboutPage = () => {
   return (
     <>
-      <h3 className={styles.title}>{"<h3>Um pouco mais de mim</h3>"}</h3>
-      <p className={styles.textLine1}>
-        {"<p>Trabalho com tecnologia tem muito tempo e sempre gostei de fazer as coisas funcionarem, me traz uma sensação de Missão cumprida. Desenvolver páginas e sistemas funcionais traz muito mais satisfação, motivando a buscar e fazer sempre o melhor possível.</p>"}
+      <h3 className={styles.title}><span className={styles.tagColor}>{"<h3>"}</span>Um pouco mais de mim<span className={styles.tagColor}>{"</h3>"}</span></h3>
+      
+      <p className={styles.textLine1}><span className={styles.tagColor}>{"<p>"}</span>
+        Trabalho com tecnologia tem muito tempo e sempre gostei de fazer as coisas funcionarem, me traz uma sensação de Missão cumprida. Desenvolver páginas e sistemas funcionais traz muito mais satisfação, motivando a buscar e fazer sempre o melhor possível.<span className={styles.tagColor}>{"</p>"}</span>
       </p>
 
-      <p className={styles.textLine2}>
-        {"<p>Trabalhar com desenvolvimento significa atualização constante. Cuidado com os detalhes, sempre atento com a qualidade, entendendo as necessidades do cliente e suprindo cada uma delas. Isso faz parte da arte do desenvolvimento.</p>"}
+      <p className={styles.textLine2}><span className={styles.tagColor}>{"<p>"}</span>
+        Trabalhar com desenvolvimento significa atualização constante. Cuidado com os detalhes, sempre atento com a qualidade, entendendo as necessidades do cliente e suprindo cada uma delas. Isso faz parte da arte do desenvolvimento.<span className={styles.tagColor}>{"</p>"}</span>
       </p>
+
+    
 
     </>
   );
